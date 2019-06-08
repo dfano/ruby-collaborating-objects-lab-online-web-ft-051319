0e2d5565c5fd7c42826d1e65a74eb7fe4fff0f95
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-08 14:08:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,13 @@ class Artist
     @@all
   end
 
+  def self.find(name)
+    self.all.find do |artist|
+      artist.name == name
+    end
+
+  end
+
   def self.find_or_create_by_name(name)
     if self.find(name) == self.find(name)
     else

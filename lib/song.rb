194c0c00d963f3d2d50ad298d290135457fbc81c
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-08 13:51:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name :artist
 
   def initialize(name)
     @name = name
+  end
+
+  # song_name.artist = "artists_name"
+  def name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end

e51911d97b713ca61cd052e42f2f534ecc13bd48
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-08 14:18:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,37 @@
 class Artist
-  attr_accessor :name, :songs
   @@all = []
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
     @@all
   end
 
-  def self.find(name)
-    self.all.find do |artist|
-      artist.name == name
-    end
+  def songs
+    Song.find_by_artist(self)
   end
 
   def self.find_or_create_by_name(name)
-    if self.find(name) == self.find(name)
-    else
-      self.create(name)
-    end
+    self.find(name) ? self.find(name) : self.create(name)
   end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
 
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
   end
 
-  # def songs
-  #   Song.find_by_artist(self)
-  # end
+  def save
+    @@all << self
+  end
 
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
